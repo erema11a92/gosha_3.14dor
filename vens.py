e084c6302062cde114c1f48fc245472c701f7aab
erema11a92 <130805858+erema11a92@users.noreply.github.com>
2023-11-11 11:18:03 +1000

    был добавлен линейный график

--- vens.py
+++ vens.py
@@ -15,8 +15,9 @@ app = dash.Dash(__name__)
 app.layout = html.Div([
     html.Div([
         html.H1('Дашборд анализа данных о количестве солнечной радиации', style={'textAlign': 'center'}),
-        html.P('Этот дашборд предоставляет информацию о количестве солнечной радиации для различных временных интервалов.',
-               style={'textAlign': 'center'}),
+        html.P(
+            'Этот дашборд предоставляет информацию о количестве солнечной радиации для различных временных интервалов.',
+            style={'textAlign': 'center'}),
         html.Div([
             html.Label('Выберите дату:', style={'fontSize': 18}),
             dcc.Dropdown(
@@ -27,8 +28,28 @@ app.layout = html.Div([
                 style={'width': '50%', 'margin': '0 auto'}
             ),
         ], style={'textAlign': 'center', 'marginBottom': '30px'}),
-    ], style={'marginBottom': '30px'})
+    ], style={'marginBottom': '30px'}),
+
+    html.Div([
+        # Линейный график
+        dcc.Graph(id='line-chart'),
+    ], style={'width': '48%', 'display': 'inline-block'})
 ])
+
+
+@app.callback(
+    Output('line-chart', 'figure'),
+    [Input('date-dropdown', 'value')]
+)
+def update_charts(selected_date):
+    # Линейный график
+    line_chart = go.Figure(go.Scatter(x=df['Дата'], y=df['Количество солнечной радиации (в Ваттах/квадратный метр)']))
+    line_chart.update_layout(title='Линейный график',
+                             xaxis_title='Дата',
+                             yaxis_title='Количество солнечной радиации (в Ваттах/квадратный метр)',
+                             plot_bgcolor='rgb(230, 230, 230)')
+    return line_chart
+
 
 # Запуск приложения
 if __name__ == '__main__':
