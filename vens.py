9f4d9988c1ac4910fe5bf06e9122725d1de80b9f
erema11a92 <130805858+erema11a92@users.noreply.github.com>
2023-11-11 11:04:13 +1000

    добавили заголовки и настройки дат

--- vens.py
+++ vens.py
@@ -4,10 +4,32 @@ from dash.dependencies import Input, Output
 import plotly.graph_objects as go
 import plotly.express as px
 import pandas as pd
+
 # Загрузка данных
 df = pd.read_csv(r"C:\Users\Admin\PycharmProjects\pythonProject\venv\ху_ня.csv")
+
 # Создание экземпляра приложения
 app = dash.Dash(__name__)
+
+# Определение структуры дашборда
+app.layout = html.Div([
+    html.Div([
+        html.H1('Дашборд анализа данных о количестве солнечной радиации', style={'textAlign': 'center'}),
+        html.P('Этот дашборд предоставляет информацию о количестве солнечной радиации для различных временных интервалов.',
+               style={'textAlign': 'center'}),
+        html.Div([
+            html.Label('Выберите дату:', style={'fontSize': 18}),
+            dcc.Dropdown(
+                id='date-dropdown',
+                options=[{'label': date, 'value': date} for date in df['Дата']],
+                value=df['Дата'].iloc[0],
+                clearable=False,
+                style={'width': '50%', 'margin': '0 auto'}
+            ),
+        ], style={'textAlign': 'center', 'marginBottom': '30px'}),
+    ], style={'marginBottom': '30px'})
+])
+
 # Запуск приложения
 if __name__ == '__main__':
     app.run_server(debug=True)
